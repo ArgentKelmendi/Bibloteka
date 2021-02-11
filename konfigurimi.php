a4c0716708804a0fcca504bccd01a783dc584cda
bardhamazreku <78913563+bardhamazreku@users.noreply.github.com>
2021-02-11 15:35:55 +0100

    Create konfigurimi.php

---
+++ konfigurimi.php
@@ -0,0 +1,11 @@
+
+<?php
+session_start();
+require 'databaza.php';
+$id = $_SESSION['perdoruesId'];
+$sql = "SELECT * FROM perdoruesit WHERE idPerdoruesit ='" . $id . "'";
+$rezultati = mysqli_query($conn, $sql);
+$stmt = mysqli_stmt_init($conn);
+$row = mysqli_fetch_array($rezultati);
+
+?>
