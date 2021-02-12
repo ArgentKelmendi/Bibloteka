fb7010f6ca0d55335dbd711675ceda4d465c4593
bardhamazreku <78913563+bardhamazreku@users.noreply.github.com>
2021-02-12 17:09:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <?php
 require '../konfigurimi.php';
 $sqlLibrat = "SELECT * FROM librat ORDER BY id desc";
-
 $rezultatiLibrat = mysqli_query($conn, $sqlLibrat);
-$stmtLibrat = mysqli_stmt_init($conn);
-$rowLibrat = mysqli_fetch_assoc($rezultatiLibrat);
-
-
-
 if(isset($_SESSION['perdoruesId'])){
 	
 ?>
-
 <!DOCTYPE html>
 <html> 
 	<head>
@@ -23,39 +16,32 @@ if(isset($_SESSION['perdoruesId'])){
 
 		<?php
 		require '../navbar.php';
+		require '../slider.php';
 		?>
-
 <h2 align="middle">Libra<p>Biblioteka e Prizrenit</p></h2>
 <?php  
-		$count = 1;
+		
 		while($rowLibrat = mysqli_fetch_assoc($rezultatiLibrat)){
-
 ?>
 		
-				
-
-
 <!-- E KE TEK NOTES KETE KOD -->
-
-				<div align="middle">
+				
 					<div class="leftcolumn">
-    					<div class="card">
-						<a href="br.php">
+    					<div align="middle" class="card">
+						<a href="librat.php?id=<?php echo $rowLibrat['id'] ?>">
 							
-							<img src ="<?php echo 'fotot/'.$rowLibrat['foto']; ?>" alt="" />
+							<img width="400" src ="<?php echo 'fotot/'.$rowLibrat['foto']; ?>" alt="" />
 							<h3><?php echo $rowLibrat["titulli"]; ?></h3>
 							<p><?php echo $rowLibrat["autori"]; ?></p>
 							<p><?php echo $rowLibrat["pershkrimi"]; ?></p>
+
 						</a>
 					</div>
 				</div>
-					</div>
-
-
 
 			<?php
 
-			$count++; } 
+			 } 
  ?>
 
 <?php
@@ -64,3 +50,6 @@ if(isset($_SESSION['perdoruesId'])){
 	header("Location: ../indexLogin.php");
 }
 ?>
+ </head>
+</body>
+
