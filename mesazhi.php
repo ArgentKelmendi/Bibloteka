30673774c8a21cbd6c62689e8d56773f301efe1a
bardhamazreku <78913563+bardhamazreku@users.noreply.github.com>
2021-02-12 20:35:01 +0100

    Update mesazhi.php

--- mesazhi.php
+++ mesazhi.php
@@ -13,13 +13,13 @@ require 'konfigurimi.php';
 if(isset($_SESSION['perdoruesId'])){
 	
 
-
 ?>
-
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	<div align="middle">	<img src="faqet/fotot/sad.png"> </div>
+<br><br><br><br>
 <body>
+	
 <h1 style="color: white" align="middle">Duhet te jeni admin per te hyre aty :'( </h1><br>
-<a  style="text-decoration: none; color: white;" href="faqet/index.php"><p align="middle">Shko ne faqen kryseore</p></a>
+<a  style="text-decoration: none; color: white;" href="faqet/index.php"><h3 align="middle">Shko ne faqen Kryesore</h3></a>
 </body>
 
 <?php 
@@ -32,7 +32,7 @@ if(isset($_SESSION['perdoruesId'])){
 <body>
 <h1 style="color: white" align="middle">Duhet te jeni te kyqur per te hyre aty :O </h1><br>
 <a  style="text-decoration: none; color: white;" href="faqet/index.php"><p align="middle">shko te faqja kycjes</p></a>
-<p align="middle">ERROR SKA PERDORES :0</p>
+<p align="middle">ERROR</p>
 </body>
 
 <?php } ?>
