12e64ce3fc2e3b1e4814febebb54d2c1c46bb1af
bardhamazreku <78913563+bardhamazreku@users.noreply.github.com>
2021-02-13 15:10:09 +0100

    Update mesazhet.php

--- mesazhet.php
+++ mesazhet.php
@@ -3,11 +3,11 @@ require 'konfigurimi.php';
 $sqlKontakt = "SELECT * FROM kontakt ORDER BY id desc";
 $rezultatiKontakt = mysqli_query($conn, $sqlKontakt);
 if($row['roli'] != "admin"){
-    header("Location: mesazhi.php");
+	header("Location: mesazhi.php");
 } else {
 ?>
 <style>
-table, td, th {
+table, td, th {  
   border: 1px solid #ddd;
   text-align: left;
 }
@@ -30,17 +30,17 @@ th, td {
 <title>Mesazhet nga Kontakt Forma</title>
 
 <div class="header">
-    <div class="logo">
-        <a href="admin.php">
-            <h1>ADMIN PANELI</h1>
-        </a>
+	<div class="logo">
+		<a href="admin.php">
+			<h1>ADMIN PANELI</h1>
+		</a>
 
 
-    <div class="logo">
-        <a href="faqet/index.php">
-            <h1 style="float: right;">KTHEHU NE HOMEPAGE</h1>
-        </a>
-    </div>
+	<div class="logo">
+		<a href="faqet/index.php">
+			<h1 style="float: right;">KTHEHU NE HOMEPAGE</h1>
+		</a>
+	</div>
 </div>
 </div> <br> <br>
 
@@ -53,9 +53,19 @@ th, td {
 <th style="color: white;"><strong>Emri</strong></th>
 <th style="color: white;"><strong>Emaili</strong></th>
 <th style="color: white;"><strong>Mesazhi</strong></th>
+<th style="color: white;"><strong>Shlyej kete Mesazh</strong></th>
 </tr>
 </thead>
 <tbody>
+
+		<?php
+			if (isset($_GET['del'])) {
+			$idDelKontakt = $_GET['del'];
+			mysqli_query($conn, "DELETE FROM kontakt WHERE id=$idDelKontakt");
+			header('location: mesazhet.php');
+		}
+	 ?>
+
 
 <?php
 $count=1;
@@ -65,6 +75,7 @@ while($rowKontakt = mysqli_fetch_assoc($rezultatiKontakt)) { ?>
 <td align="center"><p style="color: white;"><?php echo $rowKontakt["emri"]; ?></p></td>
 <td align="center"><p style="color: white;"><?php echo $rowKontakt["emaili"]; ?></p></td>
 <td align="center"><p style="color: white;"><?php echo $rowKontakt["mesazhi"]; ?></p></td>
+<td align="center"><div align="center"><a href="mesazhet.php?del=<?php echo $rowKontakt['id']; ?>"><button style="background-color: red;">Shlyej</button></a></div></td>
 </tr>
 <?php $count++; } ?>
 
