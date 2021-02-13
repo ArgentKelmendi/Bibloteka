57e922e563c5fa512f694b12cabb36e2bbad8ec5
bardhamazreku <78913563+bardhamazreku@users.noreply.github.com>
2021-02-13 21:15:30 +0100

    Create menaxhimiPerdorueseve.php

---
+++ menaxhimiPerdorueseve.php
@@ -0,0 +1,73 @@
+<link rel="stylesheet" type="text/css" href="faqet/css/adminpanel.css">
+ <table width="100%" border="1" style="border-collapse:collapse;"> 
+<?php
+require 'konfigurimi.php';
+$sqlPerdorusit = "SELECT * FROM perdoruesit ORDER BY idPerdoruesit desc";
+$rezultatiPerdoruesit = mysqli_query($conn, $sqlPerdorusit);
+
+if($row['roli'] != "admin"){
+	header("Location: mesazhi.php");
+} else {
+?>
+
+<title>Admin Paneli</title>
+
+<div class="header">
+	<div class="logo">
+		<a href="admin.php">
+			<h1>ADMIN PANELI</h1>
+		</a>
+
+	<div class="logo">
+		<a href="faqet/index.php">
+			<h1 style="float: right;">KTHEHU NE HOMEPAGE</h1>
+		</a>
+	</div>
+</div>
+</div> <br> <br>
+
+
+<div style=" text-transform: capitalize;" class="container dashboard">
+	<h1>Mirë se erdhe <?php echo $row['perdoruesiPerdoruesit']; ?>!</h1>
+
+	<thead>
+<tr>
+<th style="color: white;"><strong>Numerimi</strong></th>
+<th style="color: white;"><strong>Emri</strong></th>
+<th style="color: white;"><strong>Emaili</strong></th>
+<th style="color: white;"><strong>Nderro Rolin</strong></th>
+</tr>
+</thead>
+<tbody>
+
+	<?php 
+		if(isset($_POST['submit-menaxhim'])){
+		}
+
+	?>
+	<?php  
+		$count=1;
+		while($rowPerdorusit = mysqli_fetch_assoc($rezultatiPerdoruesit)){
+?>
+			
+	<tr><td align="center"><p style="color: white;"><?php echo $count; ?></p></td>
+		<td align="center"><p style="color: white;"><?php echo $rowPerdorusit["perdoruesiPerdoruesit"]; ?></p></td>
+		<td align="center"><p style="color: white;"><?php echo $rowPerdorusit["emailiPerdoruesit"]; ?></p></td>
+		<td align="center">
+			<form action="menaxhimiPerdorueseve.php" method="post">
+				<?php if($rowPerdorusit['roli'] == "admin"){  ?>
+			<?php echo '<button>Bej user </button>'; } else { ?>	 
+			<button>Bej Admin </button> <?php } ?>
+		</form>
+		</td>
+	</tr>
+
+			<?php
+$count++;
+			 } 
+ ?>
+
+ </tbody>
+</table>
+
+ 	<?php } ?>
