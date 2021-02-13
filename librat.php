2d0e159ba6ebb4e91377df9fd1d30d8d770d8997
ArgentKelmendi <73529127+ArgentKelmendi@users.noreply.github.com>
2021-02-13 15:00:56 +0100

    Update librat.php

--- librat.php
+++ librat.php
@@ -1,12 +1,11 @@
-
 <?php
 require '../konfigurimi.php';
 $id = $_GET['id'];
 
-	$sql = "SELECT * FROM librat WHERE id = '" . $id. "'";
-	$rezultati = mysqli_query($conn, $sql);
-	$stmt = mysqli_stmt_init($conn);
-	$rowid = mysqli_fetch_array($rezultati);
+    $sql = "SELECT * FROM librat WHERE id = '" . $id. "'";
+    $rezultati = mysqli_query($conn, $sql);
+    $rowid = mysqli_fetch_array($rezultati);
+
 
 if(isset($_SESSION['perdoruesId'])){
 
@@ -15,26 +14,45 @@ if(isset($_SESSION['perdoruesId'])){
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?php echo $rowid['titulli'] ?></title>
-	<link rel="stylesheet" type="text/css" href="css/stili2.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+    <title><?php echo $rowid['titulli'] ?></title>
+    <link rel="stylesheet" type="text/css" href="css/stili2.css">
+    <link rel="stylesheet" type="text/css" href="css/main2.css">
 </head>
 <body>
 
 <?php
-		require '../navbar.php';
-		?>
+        require '../navbar.php';
+        ?>
 <div class="mesi">
-	<h1><?php echo $rowid['titulli'] ?></h1>
-	<img width="400" height="500" src="fotot/<?php echo $rowid['foto'] ?>">
+    <h1><?php echo $rowid['titulli'] ?></h1>
+    <img width="400" height="500" src="fotot/<?php echo $rowid['foto'] ?>">
 </div>
 
 <div class="mesi">
-	<h1>Pershkrimi:</h1>
-	<p style="font-size: large;"><?php echo $rowid['pershkrimi']; ?></p>
+    <h1>Pershkrimi:</h1>
+    <p style="font-size: large;"><?php echo $rowid['pershkrimi']; ?></p>
 </div>
 
-<button style="background-color: red;">Shlyej librin</button>
+    <?php
+            if (isset($_GET['del'])) {
+            $idDel = $_GET['del'];
+            mysqli_query($conn, "DELETE FROM librat WHERE id=$idDel");
+            header('location: index.php');
+}
+     ?>
+
+<?php
+
+        if($row['roli'] == "admin"){
+
+ ?>
+
+    <div align="middle">
+        <a href="librat.php?del=<?php echo $rowid['id']; ?>"><button style="background-color: red;">Shlyej</button></a>
+    </div>
+
+<?php } ?>
+
 
 
 </body>
@@ -44,6 +62,6 @@ if(isset($_SESSION['perdoruesId'])){
 <?php
 } else {
 
-	header("Location: ../indexLogin.php");
+    header("Location: ../indexLogin.php");
 }
 ?>
